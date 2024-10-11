554b6324acd2fa9421efe41469fc67ee82b7bcf8
egjallow10 <egjallow10@gmail.com>
2024-10-11 15:21:18 +0000

    script that prints two arguments passed

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+"cript that prints a message depending of the number of arguments passed"
+const argv = process.argv.slice(2);
+//console.log(argv);
+if (argv.length === 0) {
+	console.log('No argument');
+}else {
+	console.log(`${argv[0]} is ${argv[1]}`)
+}
