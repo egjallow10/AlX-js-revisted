135192d0f0da33836811e65bad6a7576547f3a9b
egjallow10 <egjallow10@gmail.com>
2024-10-16 13:53:12 +0000

    spread arrs

---
+++ 5-spread-operator.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node 
+
+//concatenate 2 arrays and each character of a string 
+//
+export default function concatArrays(array1, array2, string) {
+	return[...array1, ...array2, ...string]
+
+}
+
