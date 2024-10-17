6e7e89a2720e72dd91e7b040c12b0f7d702bf5c8
egjallow10 <egjallow10@gmail.com>
2024-10-17 12:59:34 +0000

    function named createEmployeesObject that will receive two arguments

---
+++ 11-createEmployeesObject.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+//Write a function named createEmployeesObject that will receive two arguments
+
+export default function createEmployeesObject(departmentName, employees) {
+	return {
+		departmentName:[
+			...employees
+		]
+	}
+}
