4849666691bd4ff2310d42605283b201be129699
egjallow10 <egjallow10@gmail.com>
2024-10-17 14:55:19 +0000

    return employee

---
+++ 12-createReportObject.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+export default function createReportObject(employeesList) {
+	return{
+		allEmployees:{
+			
+			...employeesList
+		},
+		getNumberOfDepartments() {
+			return Object.keys(employeesList).length;
+		},
+	};
+
+}
