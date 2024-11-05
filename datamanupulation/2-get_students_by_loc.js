d7b211f4554a9ac04a743ede7781324e462f70a9
egjallow10 <egjallow10@gmail.com>
2024-11-05 07:50:49 +0000

    fix the erro

--- datamanupulation/2-get_students_by_loc.js
+++ datamanupulation/2-get_students_by_loc.js
@@ -1,10 +1,7 @@
-const getStudentsByLocation= (getListStudents, city) => {
-	//const returnArr = [];
-	const result = getListStudents.filter((element)=>{
-		console.log(element.location)
-		element.location === city;
+const getStudentsByLocation= (array, city) => {
+	return array.filter((ele)=>{
+		ele.location === city;
 	})
-	return result;
 }
 export default getStudentsByLocation;
 	
