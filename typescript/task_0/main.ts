2ae454774125ffdecf6f3f66d220daf6db00abcd
egjallow10 <egjallow10@gmail.com>
2024-11-18 10:49:45 +0000

    commit

---
+++ typescript/task_0/main.ts
@@ -0,0 +1,14 @@
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const student1 : Student = {firstName: "Ebrima", lastName: "jallow", age: 10, locatio: "Busumbala"}
+
+const student2 : Student = {firstName: "Buba", lastName:"Manneh", age: 30, location: "Buniadu"}
+
+//console.log(student1, student2)
+
+const studentsList: Array<Student>[student1, student2]
