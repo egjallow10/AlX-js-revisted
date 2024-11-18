767beb4144663745f86dbfbb0fca2510613838bb
egjallow10 <egjallow10@gmail.com>
2024-11-18 10:59:07 +0000

    fix errors

--- typescript/task_0/js/main.ts
+++ typescript/task_0/js/main.ts
@@ -5,10 +5,10 @@ interface Student {
 	location: string;
 }
 
-const student1 : Student = {firstName: "Ebrima", lastName: "jallow", age: 10, locatio: "Busumbala"}
+const student1 : Student = {firstName: "Ebrima", lastName: "jallow", age: 10, location: "Busumbala"}
 
 const student2 : Student = {firstName: "Buba", lastName:"Manneh", age: 30, location: "Buniadu"}
 
 //console.log(student1, student2)
 
-const studentsList: Array<Student>[student1, student2]
+const studentsList: Array<Student> =[student1, student2]
