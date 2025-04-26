b9da6037bb97650031c84fd56b48bffcb8240515
egjallow10 <egjallow10@gmail.com>
2025-04-26 07:15:58 +0000

    C program that prints

---
+++ 0x00. C - Hello, World/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *program that prints s string
+  *
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return(0);
+}
+
+
+
