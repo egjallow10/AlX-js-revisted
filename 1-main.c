78951d05d659e00bc6f3ffb8e0cd88a34b77b3d0
egjallow10 <egjallow10@gmail.com>
2025-07-19 15:30:53 +0200

    Changes

--- 1-main.c
+++ 1-main.c
@@ -6,8 +6,8 @@ int main(void)
     char c;
     int n;
 
-    printf("Address of variable 'c': %p", &c);
-     printf("Address of variable 'c': %p", &c);
+    printf("Address of variable 'c': %p\n", &c);
+     printf("Address of variable 'n': %p\n", &n);
     return(0);
 }
 
