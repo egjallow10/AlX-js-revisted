ed7c24e30f2eb0699010c4253a216dee15059f14
egjallow10 <egjallow10@gmail.com>
2025-07-19 15:27:42 +0200

    checking the memory

---
+++ 1-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    char c;
+    int n;
+
+    printf("Address of variable 'c': %p", &c);
+     printf("Address of variable 'c': %p", &c);
+    return(0);
+}
+
