c4a5023fe681dcec0ee4e5c8f898cdebd4303d9a
egjallow10 <egjallow10@gmail.com>
2025-07-29 12:21:38 +0000

    make changes

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,18 +1,42 @@
 #include "main.h"
 
+/**
+ * print_number - prints an integer using only _putchar
+ * @n: the integer to print
+ */
+void print_number(int n)
+{
+    if (n < 0)
+    {
+        _putchar('-');
+        n = -n;
+    }
 
+    if (n / 10)
+        print_number(n / 10);
+
+    _putchar((n % 10) + '0');
+}
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to the array
+ * @n: number of elements to print
+ */
 void print_array(int *a, int n)
 {
-	int i = 0;
-	while(i < n)
-	{
-		if(i != n)
-		{
-			printf("%d", a[i]);
-			printf("%c", ',');
-			printf("%c", ' ');
-			i++;
-		}
-	}
-	printf("%c", '\n');
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        print_number(a[i]);
+
+        if (i < n - 1)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
+    }
+
+    _putchar('\n');
 }
