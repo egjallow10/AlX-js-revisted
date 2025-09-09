e8a7ca4f21d9d9b978c37e93b6520f4e4ccaa27f
egjallow10 <egjallow10@gmail.com>
2025-09-09 14:13:28 +0000

    incomplete

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+
+int main(int argc, char *agrv[])
+{
+	printf("%s",argv[0]);
+	return 0;
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,13 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+#elif defined(__GNUC__)
+# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#else
+# define UNUSED(x) x
+#include <stdio.h>
 int _putchar(char c);
 
 #endif
