7d10c76c02a6115ee77f8f9463b70d58f425ae12
egjallow10 <egjallow10@gmail.com>
2025-09-13 17:21:02 +0200

    program that prints all command-line arguments, one per line,

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,12 +1,6 @@
 #ifdef MAIN_H
-#define MAIN_H
+#dfine MAIN_H
 
-#elif defined(__GNUC__)
-# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
-#elif defined(__LCLINT__)
-# define UNUSED(x) /*@unused@*/ x
-#else
-# define UNUSED(x) x
 #include <stdio.h>
 int _putchar(char c);
 
