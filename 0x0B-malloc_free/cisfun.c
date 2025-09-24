fd3321fe54d55294964ff2f3ff483c56ce23ce3e
egjallow10 <egjallow10@gmail.com>
2025-09-24 16:04:21 +0000

    memory alloaction auto

---
+++ 0x0B-malloc_free/cisfun.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * cisfun - function used for concept introduction
+ * @n1: number of projects
+ * @n2: number of tasks
+ *
+ * Return: nothing.
+ */
+
+
+
+void cisfun(unsigned int n1, unsigned int n2)
+{
+	int n;
+	char c;
+	int *ptr;
+	char array[3];
+}
