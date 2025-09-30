88595be2cdf3fbfea59a98e4d151efad88998161
egjallow10 <egjallow10@gmail.com>
2025-09-30 13:45:41 +0000

    unction that creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,17 @@
 #include "main.h"
-
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c)
 {
+	char *memo;
+
+	memo = malloc(size * sizeof(c));
+
+	if(memo == NULL)
+		return (NULL);
+
+
+	return memo;
+	
 
 }
