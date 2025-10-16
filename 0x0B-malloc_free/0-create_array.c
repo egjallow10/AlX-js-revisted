6342076e20c9cd14747252d1c704728ed5d1b1b7
egjallow10 <egjallow10@gmail.com>
2025-10-16 18:38:03 +0000

    modification of create araay function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,12 +3,21 @@
 
 char *create_array(unsigned int size, char c)
 {
+
 	char *memo;
+	unsigned int i;
+
+	if (size == 0)
+		return 0;
 
 	memo = malloc(size * sizeof(c));
 
 	if(memo == NULL)
 		return (NULL);
+
+
+	for(i = 0; i < size; i++)
+		memo[i] = c;
 
 
 	return memo;
---
+++ 0x0B-malloc_free/beej.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	int *p = malloc(sizeof(int));
+
+	*p = 12;
+	printf("%d\n", *p);
+	free(p);
+
+	*p = 3490;
+
+	printf("%d", *p);
+
+	return (0);
+}
