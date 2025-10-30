9d79e27824d184d31df0010d2079d5bef3550748
egjallow10 <egjallow10@gmail.com>
2025-10-30 19:25:37 +0100

    print format of dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "dog.h"
+
+
+
+void print_dog(struct dog *d)
+{
+    if( d == 0)
+        return;
+
+    if( d->name == 0)
+        d->name = "(nil)";
+
+    if(d->owner == 0)
+        d->owner = "(nil)";
+
+    printf("Name: %s\n",d->name);
+    printf("Age: %f\n", d->age);
+    printf("Owner: %s\n", d->owner);
+}
