adb47b73bc78024d3232b67b100c5f2bbe3d061c
egjallow10 <egjallow10@gmail.com>
2025-10-30 09:44:03 +0100

    dogs struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct fo dog
+ * @d: pointer to struct dog
+ * @name: dogs name
+ * @owner: dogs owner 
+ * 
+ */
+
+struct dog
+{
+    char *name;
+    char *ownwer;
+    float age;
+};
+
+
+#endif
