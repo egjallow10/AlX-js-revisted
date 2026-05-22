0ee512f315e8128a71f1e1db7dc3ca96c661b337
egjallow10 <egjallow10@gmail.com>
2026-05-22 15:22:20 +0000

    program that imports the function

---
+++ python/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+  print("{} + {} = {}".format(a, b, a + b))
+
+if __name__ == "__main__":
+  add(1, 2)
---
+++ python/linklist.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct studentlist
+{
+  char *name;
+  struct studentlist *next;
+} studentlist;
+
+
