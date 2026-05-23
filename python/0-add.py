b95098a26ba1cf5fb0e0778f5d82505905bb5a0f
egjallow10 <egjallow10@gmail.com>
2026-05-23 10:52:49 +0000

    fix the import issue

--- python/0-add.py
+++ python/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import add_0
 
-def add(a, b):
-  print("{} + {} = {}".format(a, b, a + b))
+a = 1
+b = 2
 
 if __name__ == "__main__":
-  add(1, 2)
+  print("{} + {} = {}".format(a, b, add_0.add(a,b)))
+
---
+++ python/0-import_add.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+__import__("0-add")
