4a31cc722c89a2d066815167b295cd7067a107ba
egjallow10 <egjallow10@gmail.com>
2026-06-21 12:50:07 +0000

    function to add unique numbers

---
+++ python/0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,6 @@
+def uniq_add(my_list=[]):
+    unique_elements = []
+    for num in my_list:
+        if num not in unique_elements:
+            unique_elements.append(num)
+    return sum(unique_elements)
